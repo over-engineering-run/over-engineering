ad384cf4872b9c4dac37c257f2574da38c61a7ad
kayac-chang <egg734631@gmail.com>
2022-05-31 14:15:19 +0800

    feat: 🎸 add two

--- main.ts
+++ main.ts
@@ -2,28 +2,38 @@ import {
   DOMParser,
   Element,
   HTMLDocument,
-  Node,
 } from "https://deno.land/x/deno_dom@v0.1.21-alpha/deno-dom-wasm.ts";
 
 type Fn<A, B> = (a: A) => B;
 interface Option<A> {
+  isNone(): boolean;
   map<B>(fn: Fn<A, B>): Option<NonNullable<B>>;
   unwrap(): A;
 }
 const Some = <A>(value: A): Option<A> => ({
+  isNone: () => false,
   map: <B>(fn: Fn<A, B>) => Option.of(fn(value)),
   unwrap: () => value,
 });
 const None = <A>(value: A): Option<A> => ({
+  isNone: () => true,
   map: <B>(_fn: Fn<A, B>) => Option.of(value as unknown as B),
   unwrap: () => value,
 });
 const Option = {
   isNone: <A>(value: A) => value === undefined || value === null,
+
   of: <A>(value: A) =>
     (Option.isNone(value) ? None(value) : Some(value)) as Option<
       NonNullable<A>
     >,
+
+  match:
+    <X, Y, F extends { none: Fn<A, X>; some: Fn<A, Y> }, A>(handle: F) =>
+    (option: Option<A>) =>
+      option.isNone()
+        ? handle.none(option.unwrap())
+        : handle.some(option.unwrap()),
 };
 
 const DOM = {
@@ -32,67 +42,48 @@ const DOM = {
     return (source: string) => it.parseFromString(source, "text/html");
   })(),
 
-  selectAll:
-    <T extends Node>(selector: string) =>
-    (el: HTMLDocument) =>
-      Array.from(el.querySelectorAll(selector)) as T[],
+  select: (selector: string) => (el: Element | HTMLDocument) =>
+    el.querySelector(selector),
+};
 
-  select: (selector: string) => (el: Element) => el.querySelector(selector),
-  text: (el: Element) => el.textContent,
-  href: (el: Element) => el.getAttribute("href"),
+const Task = {
+  wait: (ms: number) => new Promise((resolve) => setTimeout(resolve, ms)),
 };
 
 const IO = {
   text: (res: Response) => res.text(),
-  log: <T>(arg: T) => (console.log(arg), arg),
+  trace: <T>(arg: T) => (console.log(arg), arg),
+  log:
+    (msg: string) =>
+    <T>(arg: T) => (console.log(msg), arg),
+  fetch: (url: string) => () => fetch(url),
 };
 
-const List = {
-  map:
-    <A, B>(fn: Fn<A, B>) =>
-    (list: A[]) =>
-      list.map(fn),
-  head: <A>(list: A[]) => list[0],
+const MATH = {
+  random: (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.random() * (max - min) + min;
+  },
 };
 
-const String = {
-  trim: (text: string) => text.trim(),
-  match: (regex: RegExp) => (text: string) => text.match(regex),
-};
+const next = (page: number): Promise<number> =>
+  Task.wait(MATH.random(500, 700))
+    .then(IO.log(`Start: fetch ${page}`))
+    .then(IO.fetch(`https://ithelp.ithome.com.tw/2021ironman?page=${page}`))
+    .then(IO.log(`Success: fetch ${page}`))
+    .then(IO.text)
+    .then((text) =>
+      Option.of(text)
+        //
+        .map(DOM.parse)
+        .map(DOM.select('a[rel="next"]'))
+    )
+    .then(
+      Option.match({
+        some: () => next(page + 1),
+        none: () => page,
+      })
+    );
 
-const getTextFrom = (selector: string, node: Element) =>
-  Option.of(node)
-    //
-    .map(DOM.select(selector))
-    .map(DOM.text)
-    .map(String.trim);
-
-const getHrefFrom = (selector: string, node: Element) =>
-  Option.of(node)
-    //
-    .map(DOM.select(selector))
-    .map(DOM.href)
-    .map(String.trim);
-
-fetch("https://ithelp.ithome.com.tw/2021ironman")
-  .then(IO.text)
-  .then((text) =>
-    Option.of(text)
-      .map(DOM.parse)
-      .map(DOM.selectAll<Element>(".ir-index__list .ir-list"))
-      .map(
-        List.map((node) => ({
-          title: getTextFrom(".ir-list__title", node).unwrap(),
-          href: getHrefFrom(".ir-list__title > a", node).unwrap(),
-          series: getTextFrom(".ir-list__group-topic", node).unwrap(),
-          genre: getTextFrom(".ir-list__group > a", node).unwrap(),
-          author: getTextFrom(".ir-list__name", node).unwrap(),
-          publish_at: getTextFrom(".ir-list__info", node)
-            .map(String.match(/\d{4}-\d{2}-\d{2}/))
-            .map(List.head)
-            .unwrap(),
-        }))
-      )
-      .unwrap()
-  )
-  .then(IO.log);
+next(1).then(console.log);
