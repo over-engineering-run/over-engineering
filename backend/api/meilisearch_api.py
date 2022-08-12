fcac1d5b3861026ab8f20b139e8929f96b75bb57
Chin-Ho Lin <tainvecs@gmail.com>
2022-08-12 17:16:20 +0800

    feat: update api server and add index and search for doc

--- backend/api/meilisearch_api.py
+++ backend/api/meilisearch_api.py
@@ -1,5 +1,7 @@
 import os
 import json
+import time
+import datetime
 
 from flask import Flask, request, Response, abort
 import meilisearch as ms
@@ -61,24 +63,24 @@ class MeilisearchAPIServer():
         self.add_endpoint(
             endpoint="/v1/search",
             endpoint_name="search_docs",
-            handler=self.search,
+            handler=self.search_docs,
             handler_params={"index": self.ms_docs_index},
             req_methods=["GET"]
         )
-        self.add_endpoint(
-            endpoint="/v1/index/auto-complete",
-            endpoint_name="index_keywords",
-            handler=self.index,
-            handler_params={"index": self.ms_keywords_index},
-            req_methods=["POST"]
-        )
-        self.add_endpoint(
-            endpoint="/v1/search/auto-complete",
-            endpoint_name="search_keywords",
-            handler=self.search,
-            handler_params={"index": self.ms_keywords_index},
-            req_methods=["GET"]
-        )
+        # self.add_endpoint(
+        #     endpoint="/v1/index/auto-complete",
+        #     endpoint_name="index_keywords",
+        #     handler=self.index,
+        #     handler_params={"index": self.ms_keywords_index},
+        #     req_methods=["POST"]
+        # )
+        # self.add_endpoint(
+        #     endpoint="/v1/search/auto-complete",
+        #     endpoint_name="search_keywords",
+        #     handler=self.search_keywords,
+        #     handler_params={"index": self.ms_keywords_index},
+        #     req_methods=["GET"]
+        # )
 
     def run(self):
         self.app.run(debug=self.debug, port=self.port, host=self.host)
@@ -106,25 +108,93 @@ class MeilisearchAPIServer():
             headers={"Content-Type": "application/json"}
         )
 
-    def search(self, params: dict):
+    def search_docs(self, params: dict):
 
         # parse and check request
-        query = request.args.get('q')
-        if not query:
-            self.app.logger.error("Missing query in search request.")
+        req_args_key_set = set(request.args.keys())
+        req_must_key_set = {'q', 'page', 'limit'}
+
+        if (req_must_key_set - req_args_key_set) != set():
+            self.app.logger.error("Missing params {} in doc search request.".format(
+                req_must_key_set - req_args_key_set
+            ))
             return Response(status=400, headers={})
+
+        query = request.args.get('q', type=str)
+        page = request.args.get('page', type=int)
+        limit = request.args.get('limit', type=int)
+        hashtags = request.args.getlist('hashtags', type=str)
+
+        # meilisearch sdk request
+        ms_request = {
+            'offset': page * limit,
+            'limit':  limit
+        }
+
+        raw_filter = []
+        if len(hashtags) > 0:
+            for h in hashtags:
+                raw_filter.append("hashtags = {}".format(h))
+        ms_request['filter'] = ' AND '.join(raw_filter)
 
         # run search
         ms_index = params.get('index', None)
         if not ms_index:
             return Response(status=500, headers={})
 
-        resp = self.ms_client.index(ms_index).search(query)
+        raw_resp = self.ms_client.index(ms_index).search(
+            query,
+            opt_params=ms_request
+        )
+        if not raw_resp:
+            self.app.logger.error("Failed to run search request on Meilisearch.")
+            return Response(status=500, headers={})
+
+        # parse raw search response
+        resp = {
+            'query': raw_resp['query'],
+            'total': raw_resp['estimatedTotalHits'],
+            'result': []
+        }
+        for i, raw_hit in enumerate(raw_resp['hits']):
+
+            hit = {
+
+                'position': i,
+                'title':    raw_hit['title'],
+                'link':     raw_hit['href'],
+                'lastmod':  str_to_unix_time(raw_hit['published_at'], "%Y-%m-%d %H:%M:%S"),
+
+                'about_this_result': {
+
+                    'author': {
+                        'name': raw_hit['author_name'],
+                        'link': raw_hit['author_href']
+                    },
+                    'series': {
+                        'name': raw_hit['series_name'],
+                        'link': raw_hit['series_href']
+                    },
+
+                    "hashtags":     raw_hit['hashtags'],
+                    "keywords":     raw_hit['keywords'],
+                    "reading_time": raw_hit['reading_time']
+                }
+            }
+            resp['result'].append(hit)
+
         return Response(
             response=json.dumps(resp),
             status=200,
             headers={"Content-Type": "application/json"}
         )
+
+
+def str_to_unix_time(time_str:str, format_str: str):
+    return int(time.mktime(datetime.datetime.strptime(time_str, format_str).timetuple()))
+
+def calculte_reading_time():
+    pass
 
 
 if __name__ == "__main__":
