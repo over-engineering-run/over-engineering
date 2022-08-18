9c3710d7eab6839e6ded30496f70df39306872fb
Chin-Ho Lin <tainvecs@gmail.com>
2022-08-18 11:49:18 +0800

    refactor: rename filenames

--- nlp/scripts/nlp_setup.sh
+++ nlp/scripts/nlp_setup.sh
@@ -1,4 +1,4 @@
 NLP_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd -P;))"
 
 python -m pip install --upgrade pip
-python -m pip install -r "$NLP_ROOT/deployment/requirements.txt"
+python -m pip install -r "$NLP_ROOT/requirements.txt"
