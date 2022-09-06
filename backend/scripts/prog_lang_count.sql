642416a0dc0f070cf2fbb4801d3211fd786c8508
Chin-Ho Lin <tainvecs@gmail.com>
2022-09-06 15:09:37 +0800

    chore: update prog_lang_count sql func creation script

--- backend/scripts/prog_lang_count.sql
+++ backend/scripts/prog_lang_count.sql
@@ -1,16 +1,16 @@
 CREATE OR REPLACE FUNCTION prog_lang_count(top_n int)
 
-    RETURNS TABLE (prog_lang text, prog_freq int)
+    RETURNS TABLE (prog_lang text, "count" int)
     AS
     $$
 
-      WITH prog_lang_flat AS (SELECT UNNEST(programming_languages) AS prog_lang FROM articles)
+        WITH prog_lang_flat AS (SELECT UNNEST(programming_languages) AS prog_lang FROM articles)
 
-      SELECT prog_lang, COUNT(*) AS prog_freq
-      FROM prog_lang_flat
-      GROUP BY prog_lang
-      ORDER BY COUNT(*) DESC, prog_lang
-      LIMIT top_n;
+        SELECT prog_lang, COUNT(*) AS "count"
+        FROM prog_lang_flat
+        GROUP BY prog_lang
+        ORDER BY COUNT(*) DESC, prog_lang
+        LIMIT top_n;
 
     $$
     language sql;
